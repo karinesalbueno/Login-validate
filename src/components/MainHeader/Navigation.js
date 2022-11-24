3a944b4f5c58b290e471db5e611aa0cd1020323f
Karine <karine.costa@somagrupo.com.br>
2022-11-24 20:20:00 -0300

    useContext para validação de rota

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,29 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react'
+import AuthContext from '../../services/AuthContext'
 
-import classes from './Navigation.module.css';
+import classes from './Navigation.module.css'
 
 const Navigation = (props) => {
+  const context = useContext(AuthContext)
+
   return (
     <nav className={classes.nav}>
       <ul>
-        {props.isLoggedIn && (
+        {context.isLoggedIn && (
           <li>
             <a href="/">Users</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {context.isLoggedIn && (
           <li>
             <a href="/">Admin</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {context.isLoggedIn && (
           <li>
             <button onClick={props.onLogout}>Logout</button>
           </li>
         )}
       </ul>
     </nav>
-  );
-};
+  )
+}
 
-export default Navigation;
+export default Navigation
